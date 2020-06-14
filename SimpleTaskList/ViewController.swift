63fc346c7d9dfde600f9c30ec5cd2a6c5bc9622d
alex-babych <ababich.dev@gmail.com>
2020-06-14 08:49:36 +0300

    Initial Commit

---
+++ SimpleTaskList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleTaskList
+//
+//  Created by Alex Babich on 14.06.2020.
+//  Copyright © 2020 Alex Babich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
